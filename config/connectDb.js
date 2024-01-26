b52938449b44297d0df2796032d3babdfc77e751
Anchal <anchalpaswan10@gmail.com>
2024-01-26 17:31:46 +0530

    update config file

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
+
+mongoose.set('strictQuery', false);
+
 const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
